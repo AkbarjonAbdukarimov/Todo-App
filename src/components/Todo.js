5999a24f483481461cf35f732f3835afb5a3ac9a
AkbarjonAbdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2022-07-25 17:43:02 +0500

    delete complete start edit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 class Todo extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = -this.handleEdit.bind(this);
+  }
+  handleEdit() {}
+  handleDelete() {
+    this.props.delete(this.props.id);
+  }
   render() {
     return (
       <div>
         <p>{this.props.task}</p>
-        <button onClick={this.handleEdit}>Edit</button>
+        {/* <button onClick={this.handleEdit}>Edit</button> */}
         <button onClick={this.handleDelete}>X</button>
       </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,6 +9,7 @@ class TodoList extends Component {
       todos: [{ task: "Clean House", status: false, id: uuidv4() }],
     };
     this.addTodo = this.addTodo.bind(this);
+    this.delete = this.delete.bind(this);
   }
   addTodo(task) {
     if (task.task.length > 0) {
@@ -16,11 +17,21 @@ class TodoList extends Component {
       this.setState((st) => ({ todos: [...st.todos, newTask] }));
     } else alert("You Cannot Submit Empty Task!!!");
   }
+  delete(id) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id !== id),
+    });
+  }
   render() {
     return (
       <div>
         {this.state.todos.map((todo) => (
-          <Todo key={todo.id} task={todo.task} />
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            task={todo.task}
+            delete={this.delete}
+          />
         ))}
         <Form addTodo={this.addTodo} />
       </div>
