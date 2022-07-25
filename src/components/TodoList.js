9fb16b29998381c6ca1074b47b76beab21f3bfb3
AkbarjonAbdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2022-07-25 16:55:45 +0500

    working on form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Todo from "./Todo";
 import { v4 as uuidv4 } from "uuid";
 class TodoList extends Component {
   constructor(props) {
@@ -7,4 +8,10 @@ class TodoList extends Component {
       todos: [{ task: "Clean House", status: false, id: uuidv4() }],
     };
   }
+  render() {
+    return this.state.todos.map((todo) => (
+      <Todo key={todo.id} task={todo.task} />
+    ));
+  }
 }
+export default TodoList;
